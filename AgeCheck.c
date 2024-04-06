0f4162cee2cd34525afd25fa6e3179921a0190dd
AlexR8878 <146139971+AlexR8878@users.noreply.github.com>
2024-04-06 00:06:53 -0400

    Add files via upload
    #New Updated Version

---
+++ AgeCheck.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "AgeCheck.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* ageCheck(char input[]) {
+    if (strcmp(input, "Y") == 0 || strcmp(input, "y") == 0) {
+        return "You are eligible to use the application.\n";
+    }
+    else if (strcmp(input, "N") == 0 || strcmp(input, "n") == 0) {
+        return "You are not eligible to use the application.\n";
+    }
+    else {
+        return "Invalid input.\n";
+    }
+}
+
+// Function to prompt the user and check eligibility
+void checkEligibility() {
+    char ageInput[3]; // Changed to array for string input
+    printf("+---*---*---*---SLOTS---*---*---*---*---+\n");
+    printf("\n");
+    printf("Are you above the legal age of 19? (Y/N): ");
+    scanf("%2s", ageInput); // Read a single character
+
+    const char* result = ageCheck(ageInput); // Call ageCheck and capture its result
+
+    // Print the result
+    printf("%s", result);
+}
